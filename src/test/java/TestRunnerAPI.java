20ba600d79d2e5210f1e904381c2f19d9da799e0
Purushotham Maladi <maladipurushotham@gmail.com>
2018-07-22 02:12:45 -0500

    first commit

---
+++ src/test/java/TestRunnerAPI.java
@@ -0,0 +1,20 @@
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+				features= "src/Feature/Sample.feature"
+				)
+/*
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(format = {"pretty"}, monochrome=true, features = "src/Feature/Sample.feature")
+*/
+
+
+public class TestRunnerAPI {
+
+}
